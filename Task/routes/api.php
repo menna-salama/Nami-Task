957ebcad0ddf45666f4d32d57ec2d03f8e01f660
Menna Salama <mennasalama196@gmail.com>
2025-10-07 18:37:36 +0300

    last edit

--- Task/routes/api.php
+++ Task/routes/api.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Minimal API route to ensure the file exists and can be required by Laravel
-Route::get('/health', function () {
-    return response()->json(['status' => 'ok']);
-});
+ 
 
 
